8da94fea7cee95818d3701dba3478b718e567e1a
Tom Bloor <5075314+tombloor@users.noreply.github.com>
2025-07-01 22:38:06 -0500

    add jest for unit testing

--- bots/smokey-bot/src/main.ts
+++ bots/smokey-bot/src/main.ts
@@ -1,7 +1,6 @@
-import { get_message } from "./test";
+import { get_message } from "./abc";
 
 export const loop = () => {
   console.log(`Current game tick is ${Game.time}`);
-
   console.log(get_message());
 };
