9349cfd0dd563fc35fd7192840f46328ab494686
Tom Bloor <5075314+tombloor@users.noreply.github.com>
2026-07-24 18:01:24 -0500

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+"use strict";
+
+import clear from 'rollup-plugin-clear';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import screeps from 'rollup-plugin-screeps';
+import typescript from 'rollup-plugin-typescript2';
+
+let cfg;
+const dest = process.env.DEST;
+if (!dest) {
+  console.log("No destination specified - code will be compiled but not uploaded");
+} else if ((cfg = require("./screeps.json")[dest]) == null) {
+  throw new Error("Invalid upload destination");
+}
+
+export default {
+  input: "src/main.ts",
+  output: {
+    file: "dist/main.js",
+    format: "cjs",
+    sourcemap: true
+  },
+
+  plugins: [
+    clear({ targets: ["dist"] }),
+    resolve({ rootDir: "src" }),
+    commonjs(),
+    typescript({ tsconfig: "./tsconfig.json", include: ["**/*.ts"], exclude: [] }),
+    screeps({config: cfg, dryRun: cfg == null})
+  ]
+}
